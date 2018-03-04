e8c813728cb1a32640806c9874016cd55324bf0e
nganhkhoa <ng.akhoa@yahoo.com.vn>
2018-03-04 17:45:01 +0000

    add sys_procmem

--- kernelbuild/kernel-4.4.56/arch/x86/kernel/sys_procmem.c
+++ kernelbuild/kernel-4.4.56/arch/x86/kernel/sys_procmem.c
@@ -15,9 +15,42 @@ struct proc_segs {
       unsigned long end_heap;
       // stack
       unsigned long start_stack;
-      unsigned long end_stack;
+      // unsigned long end_stack;
 };
 
 asmlinkage long sys_procmem(int pid, struct proc_segs* info) {
-      // TODO:
+      // my student info
+      info->mssv = 1611617;
+
+      // check if there is a task of pid
+      struct task_struct* ts = NULL;
+      int match = 0;
+
+      for_each_process(ts) {
+            if (ts->pid == pid) {
+                  match = 1;
+                  break;
+            }
+      }
+
+      if (match != 1) return -1;
+
+      // OK, there is a process
+      // get the mm_struct to view the memory
+      struct mm_struct* mm = ts->mm;
+
+      // code
+      info->start_code = mm->start_code;
+      info->end_code = mm->end_code;
+      // data
+      info->start_data = mm->start_data;
+      info->end_data = mm->end_data;
+      // heap
+      info->start_heap = mm->start_brk;
+      info->end_heap = mm->brk;
+      // stack
+      info->start_stack = mm->stack_start;
+      // info->end_start = mm->stack_end;
+
+      return 0;
 }
